cab57a53001d7471f77aedbd6da4765bd719b976
kiarashsafaei98 <kiarashsafaei0@gmail.com>
2021-05-17 22:30:27 +0430

    creat bs4 hw2

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// const navbar = document.getElementById("navbar_top")
+// document.body.onscroll = function(){
+//     let scrolly = document.documentElement.scrollTop
+//     if (scrolly >=100) {
+//         navbar.addClass("navbar-white")
+//     }
+//     else{
+//         navbar.removeClass("navbar-white")
+//     }
+    
+// }
